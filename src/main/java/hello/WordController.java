041dfa9c4b078cc5628fde1d1f423227674bbbc7
vinayaknbGitHub <44280403+vinayaknbGitHub@users.noreply.github.com>
2018-10-20 17:30:20 +0530

    update
    add code for check word is palindrome or anagram of palindrome

--- src/main/java/hello/WordController.java
+++ src/main/java/hello/WordController.java
@@ -5,4 +5,45 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class WordController {
     // TODO Implement the /words/{word} endpoint
+    
+     @RequestMapping("/words")
+    public WordDetails words(@RequestParam(value="word") String word) {
+    	boolean palindrome=false;
+    	boolean anagramOfPalindrom=false;
+    	
+    	if(isReverse(word)) {
+    		palindrome=true;
+    		if(canMakePalindrome(word)) {
+    			anagramOfPalindrom=true;
+    		}
+    	}else {
+    		if(canMakePalindrome(word)) {
+    			anagramOfPalindrom=true;
+    		}
+    	}
+        return new WordDetails(word,palindrome,anagramOfPalindrom);
+    }
+
+	public  boolean canMakePalindrome(String name) {
+		// TODO Auto-generated method stub
+		
+		Set<Character> oddLetters = new HashSet<>();
+		
+		for (char c : name.toCharArray()) {
+			if(!oddLetters.remove(c)) {
+				oddLetters.add(c);
+				
+			}
+			
+		}
+		return oddLetters.size()<=1;
+	}
+
+	public  boolean isReverse(String name) {
+		// TODO Auto-generated method stub
+		return name.equals(new StringBuffer(name).reverse().toString());
+	}
+
+    
+    
 }
